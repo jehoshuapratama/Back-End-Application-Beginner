c77233c95ffc5c6016ed85a598e4e442c61d1d41
jehoshuapratama <jehoshua.handoko@yahoo.com>
2022-07-26 10:14:34 +0800

    readable and writeable stream

--- filesystem/index.js
+++ filesystem/index.js
@@ -5,3 +5,24 @@ const ini = path.resolve(__dirname, 'notes.txt');
 const data = fs.readFileSync(ini, 'UTF-8');
 console.log(data);
 console.log(ini);
+
+//STREAM READABLE
+const readableStream = fs.createReadStream('./article.txt', {highWaterMark: 10});
+
+readableStream.on('readable', () => {
+    try{
+        process.stdout.write(`[${readableStream.read()}]`)
+    } catch(error){
+
+    }
+})
+
+readableStream.on('end', () => {
+    console.log('Done');
+})
+
+//STREAM WRITEABLE
+const writeableStream = fs.createWriteStream('output.txt');
+writeableStream.write('Ini merupakan teks baris pertama!\n');
+writeableStream.write('Ini merupakan teks baris kedua!\n');
+writeableStream.end('Akhir dari writable stream!');
